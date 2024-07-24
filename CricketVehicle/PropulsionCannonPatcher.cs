3c37242779cb046162f69edaadbf28e894a1aec6
Michael Neises <neisesmichael@gmail.com>
2024-07-24 15:43:34 -0500

    Fix bad prop cannon interaction

---
+++ CricketVehicle/PropulsionCannonPatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CricketVehicle
+{
+	[HarmonyPatch(typeof(PropulsionCannon))]
+	public static class PropulsionCannonPatcher
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch(nameof(PropulsionCannon.ValidateNewObject))]
+		public static bool ValidateNewObjectPrefix(PropulsionCannon __instance, GameObject go)
+		{
+			if (go.GetComponent<CricketContainer>() != null && go.GetComponent<CricketContainer>().GetComponentInParent<Cricket>() != null)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
