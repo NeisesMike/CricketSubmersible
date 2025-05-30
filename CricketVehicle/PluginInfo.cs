2a05d172999654d2a529a0c77f8e6329e8d79597
Michael Neises <neisesmichael@gmail.com>
2025-05-30 16:13:21 -0500

    Update PluginInfo.cs

--- CricketVehicle/PluginInfo.cs
+++ CricketVehicle/PluginInfo.cs
@@ -5,6 +5,6 @@ namespace CricketVehicle
     {
         public const string PLUGIN_GUID = "com.mikjaw.subnautica.cricket.mod";
         public const string PLUGIN_NAME = "CricketVehicle";
-        public const string PLUGIN_VERSION = "1.4.1";
+        public const string PLUGIN_VERSION = "1.4.2";
     }
 }
